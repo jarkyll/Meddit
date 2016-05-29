18a8680d84dd1b8d1c19ddd647402b6ffb9e08c1
Nabeel Virani <nabeelvirani@gmail.com>
2016-05-29 14:52:40 -0500

    forgot mongoose connect

--- reddit/app.js
+++ reddit/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 require("./models/Posts")
 require("./models/Comments")
+mongoose.connect('mongodb://localhost/Meddit')
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
